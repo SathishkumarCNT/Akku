6bb55e3f12b16f620b5d5f2e2fc7da4e0440f1a2
mallikarjuna.d <mallikarjuna.d@cloudnowtech.com>
2017-12-11 10:51:50 +0000

    TestLinkIntegration.java edited online with Bitbucket

--- src/test/java/com/app/akku/work/common/TestLinkIntegration.java
+++ src/test/java/com/app/akku/work/common/TestLinkIntegration.java
@@ -6,7 +6,7 @@ import testlink.api.java.client.TestLinkAPIException;
 public class TestLinkIntegration {
 
 	public static String DEV_KEY  = "8018851a7430a5d16b0fc147780c94c9";
-	public static String TESTLINK_URL = "http://192.168.0.27/testlink-1.9.16/lib/api/xmlrpc/v1/xmlrpc.php";
+	public static String TESTLINK_URL = "http://192.168.0.34/testlink-1.9.16/lib/api/xmlrpc/v1/xmlrpc.php";
 	
 
 	public static void reportResult(String TestProject, String TestPlan, String Testcase, String Build, String Notes, String Result) throws TestLinkAPIException {
